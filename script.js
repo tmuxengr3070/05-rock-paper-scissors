1be252c77eff2a49f7fa46e94c1052fb0e6a9265
Guil H <g.hernandez07@gmail.com>
2025-02-21 10:03:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.getElementById("rock").addEventListener("click", function() {
+  console.log("You chose: Rock 🪨");
+});
 
+document.getElementById("paper").addEventListener("click", function() {
+  console.log("You chose: Paper 📄");
+});
+
+document.getElementById("scissors").addEventListener("click", function() {
+  console.log("You chose: Scissors ✂️");
+});
