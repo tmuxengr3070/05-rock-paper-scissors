c866c01625a92a28228142d6457252cf075d6e4a
Anthony McMorris <amcmorr2@asu.edu>
2025-10-19 11:51:02 +0000

    Update

--- script.js
+++ script.js
@@ -3,6 +3,23 @@ const rockButton = document.getElementById("rock");
 const paperButton = document.getElementById("paper");
 const scissorsButton = document.getElementById("scissors");
 
+// This function picks a random move for the computer
+function getComputerChoice() {
+  // Get a random number between 0 and 1
+  const randomNumber = Math.random();
+
+  // If the number is less than 0.33, return "Rock"
+  if (randomNumber < 0.33) {
+    return "Rock";
+  }
+  // If the number is less than 0.66, return "Paper"
+  if (randomNumber < 0.66) {
+    return "Paper";
+  }
+  // Otherwise, return "Scissors"
+  return "Scissors";
+}
+
 // Add event listener for rock button
 rockButton.addEventListener("click", function() {
   console.log("You chose: Rock 🪨");
